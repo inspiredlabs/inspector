a01ea5d9e237a081f7edb0bad95b0d00968546cf
Scott Phillips <alan.scott.phillips@protonmail.ch>
2022-07-02 10:50:00 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		adapter: vercel(),
 		// Override:
-		methodOverride: { allowed: ['PATCH', 'DELETE'] }
+		methodOverride: { allowed: ['PATCH', 'DELETE'] },
 
 		vite: {
       resolve: {
