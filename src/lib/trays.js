9a7b9954f23d28a154732b5085439cdd98a4d669
Scott Phillips <alan.scott.phillips@protonmail.ch>
2022-07-19 23:17:29 +0200

    Update trays.js

--- src/lib/trays.js
+++ src/lib/trays.js
@@ -3,14 +3,14 @@ export const trays = [
 		"id": 1,
 		"item": "SPANACOPITTES",
 		"portions": "12 portions",
-		"price": 14.00,
+		"price": 20.00,
 		"category": "Patisserie, Muffins and Trays"
 	},
 	{
 		"id": 2,
 		"item": "SMALL SPANACOPITTES",
 		"portions": "48 portions",
-		"price": 44.00,
+		"price": 16.00,
 		"category": "Patisserie, Muffins and Trays"
 	},
 	{
